f6552af39968239037d2317c76599ec1ae0837f1
sebastian thomas <sebastianthomas1@live.com>
2013-06-23 10:15:18 +0530

    started styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @jobs = Job.paginate(page: params[:page] , :per_page => 7)
+    @jobs = Job.paginate(page: params[:page] , :per_page => 3)
   end
 end
